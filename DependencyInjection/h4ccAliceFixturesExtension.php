78372eefb1a3f5e8350297f0f328444f08f22839
Jérémiah VALERIE <j.valerie@gibmedia.fr>
2014-07-23 16:14:19 +0000

    change config to accept many managers

--- DependencyInjection/h4ccAliceFixturesExtension.php
+++ DependencyInjection/h4ccAliceFixturesExtension.php
@@ -35,35 +35,44 @@ class h4ccAliceFixturesExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        if (empty($config['default_manager'])) {
+            $keys = array_keys($config['managers']);
+            $config['default_manager'] = reset($keys);
+        }
+
+        $defaultManagerConfig = $config['managers'][$config['default_manager']];
+
+        $currentManagerConfig = $defaultManagerConfig;
+
         // Set the default service ids, if they have not been defined in config.
-        switch($config['doctrine']) {
+        switch($currentManagerConfig['doctrine']) {
             case 'orm':
-                if(!$config['object_manager']) {
-                    $config['object_manager'] = 'doctrine.orm.entity_manager';
+                if(!$currentManagerConfig['object_manager']) {
+                    $currentManagerConfig['object_manager'] = 'doctrine.orm.entity_manager';
                 }
-                if(!$config['schema_tool']) {
-                    $config['schema_tool'] = 'h4cc_alice_fixtures.orm.schema_tool.doctrine';
+                if(!$currentManagerConfig['schema_tool']) {
+                    $currentManagerConfig['schema_tool'] = 'h4cc_alice_fixtures.orm.schema_tool.doctrine';
                 }
                 break;
             case 'mongodb-odm':
-                if(!$config['object_manager']) {
+                if(!$currentManagerConfig['object_manager']) {
                     $config['object_manager'] = 'doctrine_mongodb.odm.document_manager';
                 }
-                if(!$config['schema_tool']) {
-                    $config['schema_tool'] = 'h4cc_alice_fixtures.orm.schema_tool.mongodb';
+                if(!$currentManagerConfig['schema_tool']) {
+                    $currentManagerConfig['schema_tool'] = 'h4cc_alice_fixtures.orm.schema_tool.mongodb';
                 }
                 break;
             default:
                 throw new \InvalidArgumentException("Invalid value for 'doctrine'");
         }
 
-        $container->setAlias('h4cc_alice_fixtures.object_manager', $config['object_manager']);
-        $container->setAlias('h4cc_alice_fixtures.orm.schema_tool', $config['schema_tool']);
+        $container->setAlias('h4cc_alice_fixtures.object_manager', $currentManagerConfig['object_manager']);
+        $container->setAlias('h4cc_alice_fixtures.orm.schema_tool', $currentManagerConfig['schema_tool']);
 
         $managerConfig = array(
-            'locale' => $config['locale'],
-            'seed' => $config['seed'],
-            'do_flush' => $config['do_flush'],
+            'locale' => $currentManagerConfig['locale'],
+            'seed' => $currentManagerConfig['seed'],
+            'do_flush' => $currentManagerConfig['do_flush'],
         );
         $container->getDefinition('h4cc_alice_fixtures.manager')->replaceArgument(0, $managerConfig);
     }
